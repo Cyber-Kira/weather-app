c366ba7712c5b6cd27b78f9e55b0b1a48e4b5ed4
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-07-12 18:58:18 +0300

    feat: update css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
+import { CurrentWeather } from './components'
 import './index.css'
 
 const container = document.getElementById('root')!
@@ -10,10 +11,7 @@ const root = createRoot(container)
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<div className='font-raleway text-xl font-medium p-14 text-white'>
-				Testing
-			</div>
-			<div className='fixed inset-0 bg-[#1E213A] -z-10' />
+			<CurrentWeather />
 		</Provider>
 	</React.StrictMode>
 )
