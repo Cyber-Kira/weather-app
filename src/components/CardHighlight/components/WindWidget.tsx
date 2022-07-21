a7e4f7cfc50669f8982e0591d88248ae58c650da
Cyber-Kira <kirillkoloskov03@gmail.com>
2022-07-21 15:58:39 +0300

    refactoring: change navigation icon

--- src/components/CardHighlight/components/WindWidget.tsx
+++ src/components/CardHighlight/components/WindWidget.tsx
@@ -3,15 +3,17 @@ import { degToCompass } from '../../../lib/utils'
 
 export const WindWidget = ({ deg }: { deg: number }) => {
 	return (
-		<div className='flex items-center justify-center gap-4 mt-6'>
-			<span
-				className='material-icons text-gray'
-				style={{
-					transform: `rotate(${deg}deg)`,
-				}}
-			>
-				assistant_navigation
-			</span>
+		<div className='flex items-center justify-center gap-3 mt-6'>
+			<div className='bg-gray rounded-full p-[.375rem] grid items-center'>
+				<span
+					className='material-icons text-sm w-4 h-4 flex items-center justify-center text-lightestGray'
+					style={{
+						transform: `rotate(${deg}deg)`,
+					}}
+				>
+					navigation
+				</span>
+			</div>
 			<span className='font-raleway font-medium text-sm leading-[17px] text-lightestGray'>
 				{degToCompass(deg)}
 			</span>
